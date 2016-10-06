c5cedda4e36ceefbbcd1783bd8582a653cfd5ce8
krishansharma <krishan.sharma@bookmyshow.com>
2016-10-06 19:04:04 +0530

    Initial commit

---
+++ library/src/com/exp/Lazy.java
@@ -0,0 +1,14 @@
+package com.exp;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by krishansharma on 06/10/16.
+ */
+
+@Retention(RetentionPolicy.SOURCE) @Target(ElementType.PARAMETER)
+public @interface Lazy {
+}
---
+++ src/main/java/com/annotation/experiment/Main.java
@@ -0,0 +1,11 @@
+package com.annotation.experiment;
+
+/**
+ * Created by krishansharma on 06/10/16.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
